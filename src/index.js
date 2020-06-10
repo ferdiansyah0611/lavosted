d0da11b3dbc4d486bca486e5ff9850f883d3cc32
Ferdiansyah <47508140+ferdiansyah0611@users.noreply.github.com>
2020-06-10 23:22:17 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -208,19 +208,99 @@ class L{
     }
     design(data)
     {
-        var query;
+        var query, id, mobile, content_nav_open, content_nav_close, nav_close;
+        content_nav_open = '<div class="nav-mobile">';
+        content_nav_close = '</div>';
+        nav_close = '<div class="nav-mobile-close"></div>';
         const design = {
-            nav : function() {
-                console.log('nav');
-                query = document.querySelectorAll('nav.nav');
-                query.forEach(function(v, i){
-                    console.log(query[i].innerHTML);
-                });
+            nav : function(data)
+            {
+                const menu = function(data, resize = '')
+                {
+                    if(resize !== ''){
+                        function size_window(){
+                            var w = document.documentElement.clientWidth;
+                            var h = document.documentElement.clientHeight;
+                            console.log("Width: " + w + ", " + "Height: " + h);
+                        }
+                        window.addEventListener('resize', size_window);
+                        size_window();
+                    }
+                    window.addEventListener('load', function(){
+                        query = document.querySelectorAll('nav.nav');
+                        new L().html('body', content_nav_open + query[0].innerHTML, content_nav_close + nav_close)
+                        id = document.getElementById(data);
+                        mobile = [];
+                        mobile[0] = document.getElementsByClassName('nav-mobile')[0];
+                        mobile[1] = document.getElementsByClassName('nav-mobile-close')[0];
+                        mobile[0].style.marginLeft = '-300px';
+                        mobile[1].style.display = 'none';
+                        /*menu click*/
+                        id.addEventListener('click', function(){
+                            mobile[0].style.position = 'fixed';
+                            mobile[0].style.zIndex = '999999';
+                            mobile[0].style.backgroundColor = '#0ff';
+                            mobile[0].style.top = '0';
+                            mobile[0].style.left = '0';
+                            mobile[0].style.width = '300px';
+                            mobile[0].style.height = '100%';
+                            mobile[0].style.transition = 'all 0.1s ease 0s';
+                            mobile[0].style.marginLeft = '0';
+                            mobile[1].style.display = 'block';
+                            mobile[1].style.position = 'fixed';
+                            mobile[1].style.zIndex = '99999';
+                            mobile[1].style.backgroundColor = 'rgb(173 173 173 / 58%)';
+                            mobile[1].style.width = '100%';
+                            mobile[1].style.top = '0';
+                            mobile[1].style.left = '0';
+                            mobile[1].style.height = '100%';
+                            document.body.classList.add('nav-open');
+                        });
+                        /*overlay click*/
+                        mobile[1].addEventListener('click', function(){
+                            mobile[0].style.marginLeft = '-300px';
+                            mobile[1].style.display = 'none';
+                            document.body.classList.remove('nav-open');
+                        });
+                        /*move*/
+                        mobile[1].addEventListener('touchmove', function(e){
+                            if(e.changedTouches[0].pageX > 560){
+                            }
+                            else{
+                                mobile[0].style.marginLeft = e.changedTouches[0].pageX - mobile[1].offsetWidth + 'px';
+                                if(mobile[0].offsetLeft < -250){
+                                    mobile[0].classList.add('close');
+                                    mobile[0].style.marginLeft = '-300px';
+                                    mobile[1].style.display = 'none';
+                                    document.body.classList.remove('nav-open');
+                                }
+
+                            }
+                        });
+                        /*end move*/
+                        mobile[1].addEventListener('touchend', function(e){
+                            if(mobile[0].offsetLeft < -250){
+                                mobile[0].style.marginLeft = '-300px';
+                                mobile[1].style.display = 'none';
+                                document.body.classList.remove('nav-open');
+                            }else{
+                                mobile[0].style.marginLeft = '0';
+                            }
+                        });
+                    });
+                }
+                menu(data);
+                
             }
         };
         if(data.nav.active == true){
             this.src('link', {src : './css/__nav.css'});
-            design.nav();
+            if(data.nav.menu !== undefined){
+                design.nav(data.nav.id, data.nav.menu);
+            }
+            else{
+                design.nav(data.nav.id);
+            }
         }
     }
     /**
@@ -441,13 +521,27 @@ class L{
             form[i].appendChild(create);
         }
 	}
-	html(init, data)
+	html(init, data, tag_close = '')
 	{
         try{
-            var query = document.body.querySelectorAll(init);
-            for (var i = 0; i < query.length; i++) {
-                var before = query[i].innerHTML;
-                query[i].innerHTML = before + data;
+            if(init == 'body'){
+                if(tag_close !== ''){
+                    var query = document.body;
+                    var before = query.innerHTML;
+                    query.innerHTML = before + data + tag_close;
+                }else{
+                    var query = document.body;
+                    var before = query.innerHTML;
+                    query.innerHTML = before + data;
+
+                }
+            }
+            else{
+                var query = document.body.querySelectorAll(init);
+                for (var i = 0; i < query.length; i++) {
+                    var before = query[i].innerHTML;
+                    query[i].innerHTML = before + data;
+                }
             }
         }catch(e){
             throw Error(e);
