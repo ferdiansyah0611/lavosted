c76e4193d66c21245f59b8ee2f083612b079692f
Ferdiansyah <47508140+ferdiansyah0611@users.noreply.github.com>
2020-06-13 17:52:37 +0700

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
 	watch: true,
 	mode: 'development',
-  	entry: './src/index.js',
+  	entry: './src/js/index.js',
     devServer: {
     	contentBase: path.join(__dirname, 'dist'),
     	compress: true,
