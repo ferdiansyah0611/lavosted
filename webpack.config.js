23523871febbf26d9bef6665447ec63b02d2f91f
Ferdiansyah <47508140+ferdiansyah0611@users.noreply.github.com>
2020-06-16 04:46:53 +0700

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,6 @@ module.exports = {
     devServer: {
     	contentBase: path.join(__dirname, 'dist'),
     	compress: true,
-    	port: 9000,
-    	allowedHosts: [
-      'host.com',
-      'subdomain.host.com',
-      'subdomain2.host.com',
-      'host2.com'
-    ]
   	},
   	output: {
     filename: 'main.js',
