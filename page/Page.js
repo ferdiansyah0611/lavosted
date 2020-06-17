f40682a31839b444d1a0350412fe1e90d7ed09a4
Ferdiansyah <47508140+ferdiansyah0611@users.noreply.github.com>
2020-06-18 01:12:44 +0700

    Add files via upload

---
+++ page/Page.js
@@ -0,0 +1,39 @@
+export default{
+	template(){
+		var $app = '<p><route-link to="/" @each="' + history.state + '">TESTED<route-link></p>';
+		return $app;
+	},
+	props(){
+
+	},
+	data(){
+		var $data = [];
+		$data['category'] = {da: 'name'}
+		return $data;
+	},
+	mounted(){
+		console.log('mounted');
+	},
+	created(){
+		this.methods.dat();
+	},
+	methods : {
+		dat(){
+			L.api('https://api.apis.guru/v2/metrics.json').get({
+				cache : true,
+				start: function(){
+					console.log('start');
+				},
+				progress: function(){
+					console.log('progress');
+				},
+				success: function(data){
+					console.log(data)
+				},
+				error: function(message, status){
+					console.error(message);
+				}
+			});
+		}
+	}
+}
